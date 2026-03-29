fc88744fef620059e800e56c26586fab88c2ce78
pavan0209 <pavanasonawane@gmail.com>
2026-03-29 11:39:48 +0530

    initial commit

---
+++ src/main/java/org/pavan/di/Main.java
@@ -0,0 +1,11 @@
+package org.pavan.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
